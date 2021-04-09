cb71550294424c25f601da66b382f43eade4ed6a
Antoine Hebert <hebert.antoine@gmail.com>
2021-04-08 20:28:48 -0400

    Ch 9. Putting it together — Planes

--- src/main.rs
+++ src/main.rs
@@ -25,32 +25,19 @@ use tuple::Tuple;
 use world::World;
 
 fn main() {
-    chapter_8_shadows();
+    chapter_9_planes();
 }
+
+const WIDTH: usize = 100;
+const HEIGHT: usize = 50;
 
 //
 // Putting it together. Exercises at the end of chapters.
 //
-fn chapter_8_shadows() {
-    // Floors and walls are extremely flatten spheres...
-    let mut floor = Shape::sphere();
-    floor.transform = scaling(10.0, 0.01, 10.);
+fn chapter_9_planes() {
+    let mut floor = Shape::plane();
     floor.material.color = Color::new(1.0, 0.9, 0.9);
     floor.material.specular = 0.0;
-
-    let mut left_wall = Shape::sphere();
-    left_wall.transform = translation(0.0, 0.0, 5.0)
-        * rotation_y(-PI / 4.0)
-        * rotation_x(PI / 2.0)
-        * scaling(10.0, 0.01, 10.);
-    left_wall.material = floor.material.clone();
-
-    let mut right_wall = Shape::sphere();
-    right_wall.transform = translation(0.0, 0.0, 5.0)
-        * rotation_y(PI / 4.0)
-        * rotation_x(PI / 2.0)
-        * scaling(10.0, 0.01, 10.);
-    right_wall.material = floor.material.clone();
 
     let mut middle = Shape::sphere();
     middle.transform = translation(-0.5, 1.0, 0.5);
@@ -73,13 +60,11 @@ fn chapter_8_shadows() {
     let light = Light::new(Tuple::point(-10.0, 10.0, -10.0), WHITE);
     let mut world = World::new(light);
     world.objects.push(floor);
-    world.objects.push(left_wall);
-    world.objects.push(right_wall);
     world.objects.push(middle);
     world.objects.push(right);
     world.objects.push(left);
 
-    let mut camera = Camera::new(100, 50, PI / 3.0);
+    let mut camera = Camera::new(WIDTH, HEIGHT, PI / 3.0);
     camera.transform = view_transform(
         Tuple::point(0.0, 1.5, -5.0),
         Tuple::point(0.0, 1.0, 0.0),
